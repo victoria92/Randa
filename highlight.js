896f1b2400f820c494ece7d111f0392d730a6726
victoria92 <viki.hristova@gmail.com>
2015-02-04 19:18:28 +0200

    Add simple highlighting

--- highlight.js
+++ highlight.js
@@ -1,5 +1,29 @@
-number_regex = /(\d+.?\d?)/;
-current_word = "";
+numbers = /\b(\d+\.?\d?)\b/g;
+numFunctions = /\b(abs|sqrt|ceiling|floor|trunc|round|signif|cos|sin|tan|log|log10|exp)\b/g;
+charFunctions = /\b(substr|grep|sub|strsplit|paste|toupper|tolower)\b/g;
+statsFunctions = /\b(d|p|q|r)(norm|binom|pois|unif)\b/g;
+otherFunctions = /\b(mean|sd|median|quantile|range|sum|diff|min|max|scale|seq|rep|cut|c)\b/g;
+controlStructures = /\b(if|else|for|while|switch|ifelse)\b/g;
+boolValues = /\b(TRUE|FALSE|T|F|NULL)\b/g;
+brackets = /(\[|\]|\(|\))/g;
+
+currentWord = "";
+
+var highlightFunctions = function(text) {
+    text = text.replace(numFunctions, "<font color='blue'>" + "$1" + "</font>");
+    text = text.replace(charFunctions, "<font color='blue'>" + "$1" + "</font>");
+    text = text.replace(statsFunctions, "<font color='blue'>" + "$1" + "</font>");
+    text = text.replace(otherFunctions, "<font color='blue'>" + "$1" + "</font>");
+    return text;
+};
+
+var highlightStrings = function(text) {
+    return text.replace(/(".*?")/g, "<font color='red'>" + "$1" + "</font>");
+};
+
+var highlightComments = function(text) {
+    return text.replace(/(#.*?$)/mg, "<font color='grey'>" + "$1" + "</font>");
+};
 
 $(document).ready(function() {
     $("#main").on("keyup", "pre", function(event) {
@@ -7,10 +31,13 @@ $(document).ready(function() {
         console.log(text);
         var character = String.fromCharCode(event.which);
         if (event.which !== 0) {
-            text = text.replace(/(\[|\])/g, "<font color='green'>" + "$1" + "</font>");
-            text = text.replace(/(\d+\.?\d*)/g, "<font color='pink'>" + "$1" + "</font>");
-            text = text.replace(/(<font color=("|')\w*("|')>)+/g, "$1");
-            text = text.replace(/(<\/font>)+/g, "$1");
+            text = text.replace(brackets, "<font color='green'>" + "$1" + "</font>");
+            text = highlightComments(text);
+            text = highlightFunctions(text);
+            text = highlightStrings(text);
+            text = text.replace(numbers, "<font color='pink'>" + "$1" + "</font>");
+            text = text.replace(controlStructures, "<b>" + "$1" + "</b>");
+            text = text.replace(boolValues, "<font color='red'>" + "$1" + "</font>");
             console.log(text);
             $("#code").html(text);
         }
